578f3cfddc5c095958a05ecb9a961bcc69db7b42
hussainkeshwani61 <131788741+hussainkeshwani61@users.noreply.github.com>
2023-10-30 06:10:26 -0600

    Delete graphical-programming-language directory

--- graphical-programming-language/graphical-programming-language/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace graphical_programming_language.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
-
