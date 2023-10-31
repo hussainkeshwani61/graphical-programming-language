024306a2c7fced315dd9364cc595f2181a686bd5
hussainkeshwani61 <hussainkeshwani61@gmail.com>
2023-10-31 10:46:49 -0600

    PictureBox to watch shapes and theme color

--- graphical-programming-language/Form1.cs
+++ graphical-programming-language/Form1.cs
@@ -16,5 +16,15 @@ namespace graphical_programming_language
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
