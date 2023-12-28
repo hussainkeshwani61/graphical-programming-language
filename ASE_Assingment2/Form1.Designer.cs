99250820737213bc0b04aca971f3a73ddd2fc6bf
hussainkeshwani61 <hussainkeshwani61@gmail.com>
2023-12-28 07:33:35 -0800

    Start to implement part 2

---
+++ ASE_Assingment2/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace ASE_Assingment2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.ControlPanel = new System.Windows.Forms.RichTextBox();
+            this.commandline = new System.Windows.Forms.TextBox();
+            this.display = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.display)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 400);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 0;
+            this.button3.Text = "Run";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(104, 400);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(192, 400);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // ControlPanel
+            // 
+            this.ControlPanel.Location = new System.Drawing.Point(12, 12);
+            this.ControlPanel.Name = "ControlPanel";
+            this.ControlPanel.Size = new System.Drawing.Size(255, 240);
+            this.ControlPanel.TabIndex = 3;
+            this.ControlPanel.Text = "";
+            // 
+            // commandline
+            // 
+            this.commandline.Location = new System.Drawing.Point(12, 317);
+            this.commandline.Name = "commandline";
+            this.commandline.Size = new System.Drawing.Size(255, 20);
+            this.commandline.TabIndex = 4;
+            // 
+            // display
+            // 
+            this.display.Location = new System.Drawing.Point(290, 12);
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(452, 452);
+            this.display.TabIndex = 5;
+            this.display.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(754, 476);
+            this.Controls.Add(this.display);
+            this.Controls.Add(this.commandline);
+            this.Controls.Add(this.ControlPanel);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.display)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RichTextBox ControlPanel;
+        private System.Windows.Forms.TextBox commandline;
+        private System.Windows.Forms.PictureBox display;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
