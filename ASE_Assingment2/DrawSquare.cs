85a27b41a11982a5894fda3f6bd672d9adc5ac2b
hussainkeshwani61 <hussainkeshwani61@gmail.com>
2024-01-04 13:24:47 -0800

    add drawsquare class and implemented

---
+++ ASE_Assingment2/DrawSquare.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASE_Assingment2
+{
+    public class DrawSquare : DrawRectangle
+    {
+        readonly int size;
+        public DrawSquare(int x, int y, int size) : base(x, y, size, size)
+        {
+
+            this.size = size;
+        }
+
+
+        public override void Draw(Graphics g, Pen pen, Brush brush)
+        {
+            base.Draw(g, pen, brush);
+        }
+    }
+}
