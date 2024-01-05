2a1a43d3c3c8f5180c24984e9c57c6fa1d884d7c
hussainkeshwani61 <hussainkeshwani61@gmail.com>
2024-01-05 10:08:09 -0800

    completed unittest

---
+++ ASE_Assingment_unittest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ASE_Assingment_unittest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ASE_Assingment_unittest")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("472f16db-1f8f-4978-9cb7-2b0ccda597bf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ASE_Assingment_unittest/UnitTest1.cs
@@ -0,0 +1,70 @@
+﻿using ASE_Assingment2;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ASE_Assingment_unittest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void testIfStatements()
+        {
+            try
+            {
+                int num = 25;
+                int num2 = 35;
+                string symbol = "=";
+                if (symbol == "=")
+                {
+                    if (num == num2)
+                    {
+                        Assert.AreEqual(num, num2, 00.1, "Numbers are matched");
+                    }
+                    else
+                    {
+                        Assert.AreNotEqual(num, num2, 00.1, "Numbers are not matched");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Symbol is not correct.");
+            }
+        }
+        [TestMethod]
+        public void setParameterTest()
+        {
+            var r = new DrawRectangle();
+            int x = 200, y = 200, width = 100, height = 100;
+            r.set(x, y, height, width);
+            Assert.AreEqual(200, 400);
+        }
+
+        [TestMethod]
+        public void setParameterTest3()
+        {
+            var r = new DrawRectangle();
+            int x = 100, y = 300, width = 200, height = 400;
+            r.set(x, y, height, width);
+            Assert.AreEqual(x, y);
+        }
+
+        [TestMethod]
+        public void setParameterTest2()
+        {
+            var l = new DrawTriangle();
+            int x = 200, y = 200, toX = 200, toY = 200;
+            l.set(x, y, toX, toY);
+            Assert.AreEqual(200, y);
+        }
+        [TestMethod]
+        public void setmethodtest()
+        {
+            var circle = new DrawCircle();
+            int x = 200, y = 200, radius = 100;
+            circle.set(x, y, radius);
+            Assert.AreEqual(200, y);
+        }
+    }
+}
